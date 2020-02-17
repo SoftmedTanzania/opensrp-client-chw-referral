fcba5ffef5097c02d0a56363cc998ab1191a8148
Elly Kitoto <junkmailstoelly@gmail.com>
2020-02-17 13:34:56 +0300

    Fix codacy issue - long method

--- opensrp-chw-referral/src/test/java/org/smartregister/chw/referral/util/JsonFormUtilsTest.kt
+++ opensrp-chw-referral/src/test/java/org/smartregister/chw/referral/util/JsonFormUtilsTest.kt
@@ -22,68 +22,76 @@ class JsonFormUtilsTest {
 
     @Test
     fun `Should return the right obs objects for the event`() {
-        val mapOfValues = hashMapOf<String, NFormViewData>().also { values ->
+        val mapOfValues = getValues()
+        val list = JsonFormUtils.getObs(mapOfValues)
+        assertTrue(list.size == 2)
+        val ob1 = list[0]
+        assertEquals(ob1.fieldType, "1")
+        assertEquals(ob1.formSubmissionField, "spinner_radio_texts")
+        assertEquals(ob1.fieldCode, "id1")
+        assertEquals(ob1.parentCode, "p1")
+        assertTrue(ob1.humanReadableValues.contains("Nairobi"))
+        assertTrue(ob1.values.contains("opid1"))
+        val ob2 = list[1]
+        assertEquals(ob2.fieldType, "2")
+        assertEquals(ob2.formSubmissionField, "multiple_checkboxes")
+        assertEquals(ob2.fieldCode, "id2")
+        assertEquals(ob2.parentCode, "p2")
+        assertTrue(ob2.values.contains("opid21") && ob2.values.contains("opid22"))
+        assertTrue(
+            ob2.humanReadableValues.contains("Option 1") && ob2.humanReadableValues.contains(
+                "Option 2"
+            )
+        )
+    }
+
+    private fun getValues(): HashMap<String, NFormViewData> {
+        return hashMapOf<String, NFormViewData>().also { values ->
             values["spinner_radio_texts"] = NFormViewData().apply {
                 type = SpinnerNFormView::class.simpleName
-                this.metadata = hashMapOf(
-                    "openmrs_entity" to "1",
-                    "openmrs_entity_id" to "id1",
-                    "openmrs_entity_parent" to "p1"
+                metadata = hashMapOf(
+                    OPENMRS_ENTITY to "1",
+                    OPENMRS_ENTITY_ID to "id1",
+                    OPENMRS_ENTITY_PARENT to "p1"
                 )
                 value = NFormViewData().apply {
                     type = null
                     value = "Nairobi"
-                    this.metadata = hashMapOf(
-                        "openmrs_entity" to "op1",
-                        "openmrs_entity_id" to "opid1",
-                        "openmrs_entity_parent" to "opp1"
+                    metadata = hashMapOf(
+                        OPENMRS_ENTITY to "op1",
+                        OPENMRS_ENTITY_ID to "opid1",
+                        OPENMRS_ENTITY_PARENT to "opp1"
                     )
                 }
             }
             values["multiple_checkboxes"] = NFormViewData().apply {
                 type = MultiChoiceCheckBox::class.simpleName
-                this.metadata = hashMapOf(
-                    "openmrs_entity" to "2",
-                    "openmrs_entity_id" to "id2",
-                    "openmrs_entity_parent" to "p2"
+                metadata = hashMapOf(
+                    OPENMRS_ENTITY to "2",
+                    OPENMRS_ENTITY_ID to "id2",
+                    OPENMRS_ENTITY_PARENT to "p2"
                 )
                 value = hashMapOf<String, NFormViewData>().also {
-                  it["opt1"] =  NFormViewData().apply {
-                      type = null
-                      value = "Option 1"
-                      this.metadata = hashMapOf(
-                          "openmrs_entity" to "op21",
-                          "openmrs_entity_id" to "opid21",
-                          "openmrs_entity_parent" to "opp21"
-                      )
-                  }
-                    it["opt2"] =  NFormViewData().apply {
+                    it["opt1"] = NFormViewData().apply {
+                        type = null
+                        value = "Option 1"
+                        metadata = hashMapOf(
+                            OPENMRS_ENTITY to "op21",
+                            OPENMRS_ENTITY_ID to "opid21",
+                            OPENMRS_ENTITY_PARENT to "opp21"
+                        )
+                    }
+                    it["opt2"] = NFormViewData().apply {
                         type = null
                         value = "Option 2"
-                        this.metadata = hashMapOf(
-                            "openmrs_entity" to "op22",
-                            "openmrs_entity_id" to "opid22",
-                            "openmrs_entity_parent" to "opp22"
+                        metadata = hashMapOf(
+                            OPENMRS_ENTITY to "op22",
+                            OPENMRS_ENTITY_ID to "opid22",
+                            OPENMRS_ENTITY_PARENT to "opp22"
                         )
                     }
                 }
             }
         }
-        val list = JsonFormUtils.getObs(mapOfValues)
-        assertTrue(list.size == 2)
-        val ob1 = list[0]
-        assertEquals(ob1.fieldType,"1" )
-        assertEquals(ob1.formSubmissionField,"spinner_radio_texts" )
-        assertEquals(ob1.fieldCode,"id1" )
-        assertEquals(ob1.parentCode,"p1" )
-        assertTrue(ob1.humanReadableValues.contains("Nairobi"))
-        assertTrue(ob1.values.contains("opid1"))
-        val ob2 = list[1]
-        assertEquals(ob2.fieldType,"2" )
-        assertEquals(ob2.formSubmissionField,"multiple_checkboxes" )
-        assertEquals(ob2.fieldCode,"id2" )
-        assertEquals(ob2.parentCode,"p2" )
-        assertTrue(ob2.values.contains("opid21") && ob2.values.contains("opid22"))
-        assertTrue(ob2.humanReadableValues.contains("Option 1") && ob2.humanReadableValues.contains("Option 2"))
     }
 }
