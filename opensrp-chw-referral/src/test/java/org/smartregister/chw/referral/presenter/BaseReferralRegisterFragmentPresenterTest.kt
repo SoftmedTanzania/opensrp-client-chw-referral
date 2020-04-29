d280cdb40ef4e88ede132bf347f239dd691fc899
cozej4 <ilakozejumanne@gmail.com>
2020-04-29 12:13:15 +0300

    FIxed failing tests

--- opensrp-chw-referral/src/test/java/org/smartregister/chw/referral/presenter/BaseReferralRegisterFragmentPresenterTest.kt
+++ opensrp-chw-referral/src/test/java/org/smartregister/chw/referral/presenter/BaseReferralRegisterFragmentPresenterTest.kt
@@ -64,7 +64,7 @@ class BaseReferralRegisterFragmentPresenterTest {
     fun `Should return the due filter query`() {
         assertEquals(
             referralRegisterFragmentPresenter.getDueFilterCondition(),
-            "referral_status = ' ${Constants.BusinessStatus.REFERRED}'"
+            "task.business_status = '${Constants.BusinessStatus.REFERRED}'"
         )
     }
 
