12cd4ad7cde8c58e48f63f685597d59f0789c965
Elly Kitoto <junkmailstoelly@gmail.com>
2020-07-14 13:41:17 +0300

    Fix sample app crash
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- sample/src/main/java/org/smartregister/chw/referral/sample/activity/ClientReferralActivity.kt
+++ sample/src/main/java/org/smartregister/chw/referral/sample/activity/ClientReferralActivity.kt
@@ -1,17 +1,18 @@
 package org.smartregister.chw.referral.sample.activity
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.nerdstone.neatformcore.form.json.JsonFormBuilder
 import com.nerdstone.neatformcore.form.json.JsonFormEmbedded
 import org.smartregister.chw.referral.R
 
-class IssueReferralActivity : AppCompatActivity() {
+class ClientReferralActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.neat_referral_form_activity)
         val jsonFormBuilder = JsonFormBuilder(this, "json.form/general_neat_referral_form.json")
-        JsonFormEmbedded(jsonFormBuilder, findViewById<LinearLayout>(R.id.formLayout))
+        JsonFormEmbedded(jsonFormBuilder, findViewById<LinearLayout>(R.id.formLayout)).buildForm()
     }
 }
