e76eec5ae6cdff11d72922ae7de40d6b714653f0
Kassim Sheghembe <sheghembekassim@gmail.com>
2024-05-03 18:51:53 +0300

    Added documentation for a class

--- opensrp-chw-referral/src/main/java/org/smartregister/chw/referral/util/LocationUtils.kt
+++ opensrp-chw-referral/src/main/java/org/smartregister/chw/referral/util/LocationUtils.kt
@@ -6,11 +6,22 @@ import org.smartregister.domain.Location
 import org.smartregister.repository.LocationRepository
 import org.smartregister.repository.LocationTagRepository
 
+/**
+ * Utility class for location-related operations.
+ */
 object LocationUtils {
+    /**
+     * Recursively searches for the parent location ID that has a specific tag associated with it.
+     *
+     * @param locations A list of Location objects to search within.
+     * @param locationId The ID of the current location being searched.
+     * @param tagName The name of the tag to search for.
+     * @return The ID of the parent location that has the specified tag, or null if not found.
+     */
     private fun getParentLocationIdWithTags(
-        locations: List<Location>,
-        locationId: String,
-        tagName: String
+            locations: List<Location>,
+            locationId: String,
+            tagName: String
     ): String? {
         val locationTagReposity = LocationTagRepository()
         val allLocationTags = locationTagReposity.allLocationTags
@@ -27,11 +38,16 @@ object LocationUtils {
         return null
     }
 
+    /**
+     * Retrieves the Ward ID associated with the current location.
+     *
+     * @return The Ward ID, or null if not found.
+     */
     fun getWardId(): String? {
         val locationRepository = LocationRepository()
         val locations = locationRepository.allLocations
         val locationId = Context.getInstance().allSharedPreferences()
-            .getPreference(AllConstants.CURRENT_LOCATION_ID)
+                .getPreference(AllConstants.CURRENT_LOCATION_ID)
         return getParentLocationIdWithTags(locations, locationId, "Ward")
     }
 }
